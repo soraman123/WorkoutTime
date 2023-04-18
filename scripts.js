b244e9d6e4cbfc08fddee2cb4ca59e89023e70e7
Alvaro G <alvarogm2004@gmail.com>
2023-04-17 23:08:16 -0400

    Update all scripts to be on one page Update email API with custom message Update leg day with saving user input field and displaying on refresh

--- scripts.js
+++ scripts.js
@@ -1,41 +1,69 @@
-/* 
-Below we create an array of all card exercise names, create a function template for adding a click event,
-and then add click event to all once. This click event will toggle the 'card-selected' CSS on every click.
-This way if our card elements change, all we have to do is update the array. 
-*/
+// Test, currently only for leg day page; has changed how submit button works
 
-const allExerciseNames = [
-  'pullup',
-  'backrow',
-  'curls',
-  'shrugs',
-  'supermans',
-  'goodmornings'
-];
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('load', function() {
+    if (location.pathname === '/leg-day-page-2.html') {
+      getUserInputs();
+    }
+  });
+});
+
+function clearStorage(){
+  localStorage.clear();
+}
+
+// Created an array for each exercise day:
+
+var allLegDayExercises = ['squats','lunges','deadlifts','hipbridges','nordiccurls','calfraises'];
+var allBackDayExercises = ['pullups','backrows','curls','shrugs','supermans','goodmornings'];
+var allChestDayExercises = ['benchpress','pushups','dbflys','shoulderpress','shoulderflys','dips'];
+
+const allExerciseNames = [];
+
+// Below will update allExerciseName array, based off page name:
+
+function updateAllExerciseNames() {
+  let pageName = document.location.pathname;
+  pageName = pageName.slice(-17,-6);
+  allExerciseNames.length = 0;
+  switch(pageName) {
+    case 'g-day-page-':
+      allExerciseNames.push(...allLegDayExercises);
+      return allExerciseNames;
+    case 'k-day-page-':
+      allExerciseNames.push(...allBackDayExercises);
+      return allExerciseNames;
+    case 't-day-page-':
+      allExerciseNames.push(...allChestDayExercises);
+      return allExerciseNames;
+  }
+};
+
+updateAllExerciseNames(); //If this isnt here, code chain stops; try async?
+
+
+// Below a click event is added to all cards, based off what exercise array is selected:
 
 function addClickEventListener(eventName, cssClassToToggle, elementToAddListenerTo, elementToBeToggled) {
   document.querySelector(elementToAddListenerTo).addEventListener(eventName, () => {
     document.querySelector(elementToBeToggled).classList.toggle(cssClassToToggle);
   }); 
-}
+};
 
-allExerciseNames.forEach(function(exerciseName) {
+allExerciseNames.forEach(function(exerciseName){
   var nameOfButton = `${exerciseName}-b`;
   addClickEventListener('click','card-selected',`#${exerciseName}`,`#${nameOfButton}`);
 });
 
 
-// Below I create an array with only "card-selected" id elements.
+// Below an array with only selected elements is created, stored in local storage item 'exercises':
 
-var allSelectedCardElements = document.getElementsByClassName('card-selected'); // getElementsbyClassName will return an HTML COllection array-like item.
+var arrayOfSelectedCardIds = []; 
 
-arrayOfSelectedCardIds = [];
-
-function updateSelectedExercisesArray(){ //This function updates local storage item 'exercises'.
-
+function updateSelectedExercisesArray(){
+  var allSelectedCardElements = document.getElementsByClassName('card-selected'); 
   arrayOfSelectedCardIds = []; 
-  localStorage.removeItem('exercises');  // Reset arrayOfSelectedCardIds and local storage item 'exercises'.
-
+  localStorage.removeItem('exercises'); 
   var temporaryArray = Array.from(allSelectedCardElements); 
   temporaryArray.forEach(function(element){ 
     arrayOfSelectedCardIds.push(element.getAttribute('id'));
@@ -45,27 +73,68 @@ function updateSelectedExercisesArray(){ //This function updates local storage i
 };
 
 
+// Get user input on all non-blank fields, save as variable to be used in email API:
+
+function getDynamicMessageContent() {
+  var arrayAllSets = [];
+  updateAllExerciseNames();
+  allExerciseNames.forEach(function(exerciseName){ 
+    var setOneName = `${exerciseName}_set_1`;
+    var setOneValue = document.forms[0][setOneName].value;
+    setOneValue ? arrayAllSets.push(`${setOneName}: ${setOneValue}`) : " "; //Only add non-blank values to array.
+    var setTwoName = `${exerciseName}_set_2`;
+    var setTwoValue = document.forms[0][setTwoName].value;
+    setTwoValue ? arrayAllSets.push(`${setTwoName}: ${setTwoValue}`) : " ";
+  });
+  var arrayAllSetsModified = arrayAllSets.join('\n');
+  console.log(arrayAllSetsModified);
+  return arrayAllSetsModified; 
+};
+
+// Will update subject line of email:
+
+function getExerciseDaySelected() {
+  let pageName = document.location.pathname;
+  pageName = pageName.slice(-17,-6);
+  allExerciseNames.length = 0;
+  switch(pageName) {
+    case 'g-day-page-':
+      var exerciseDay = 'Leg Day';
+      break;
+    case 'k-day-page-':
+      var exerciseDay = 'Back Day';
+      break;
+    case 't-day-page-':
+      var exerciseDay = 'Chest Day';
+      break;
+  }
+  return exerciseDay;
+};
+
+
 // Send email when clicking submit button on Page 3.
 
 function sendEmail(){
-(function() { emailjs.init('yG52IHiSAOdJI4XSe'); })();
-window.onload = function() {
+  (function() { emailjs.init('yG52IHiSAOdJI4XSe'); })();
+  window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) { //adding type='submit' adds this event listener to that input element
-        event.preventDefault();
-          const date = new Date();
-          var currentDayName = date.toString().slice(0,4);
-          var currentDay = date.getDate();
-          var currentMonth = date.getMonth() + 1;
-          var currentYear = date.getFullYear();
-        this.date_time.value = `${currentDayName} ${currentMonth}-${currentDay}-${currentYear}`; //Let's change this to current date "Back Workout 04/04/2023"
-        emailjs.sendForm('Workout_Time_Results', 'Workout_Time_Results!', this)
-            .then(function() {
-                alert('SUCCESS! Email has been sent. Make sure to check your spam folder just in case!');
-            }, function(error) {
-                alert('FAILED...', error);
-            });
+      event.preventDefault();
+      const date = new Date();
+      var currentDayName = date.toString().slice(0,4);
+      var currentDay = date.getDate();
+      var currentMonth = date.getMonth() + 1;
+      var currentYear = date.getFullYear();
+      this.date_time.value = `${currentDayName} ${currentMonth}-${currentDay}-${currentYear}`; //Let's change this to current date "Back Workout 04/04/2023"
+      this.exercise_day.value = getExerciseDaySelected();
+      this.message_content.value = getDynamicMessageContent();
+      emailjs.sendForm('Workout_Time_Results', 'Workout_Time_Results!', this)
+        .then(function() {
+          alert('SUCCESS! Email has been sent. Make sure to check your spam folder just in case!');
+        }, function(error) {
+          alert('FAILED...Make sure you typed in an email address!', error);
+          });
     });
-}
+  }
 };
 
 
@@ -83,44 +152,92 @@ function hideAllBackDayCards(){
   })
 };
 
+// Below is function for index.html buttons:
 
-// Add click event to Next Button, to run updateSelectedExercisesArray and then take us to page 3.
+function goToPageOne(button){
+  if (button.classList.contains("legButton")) {
+    window.location.href='leg-day-page-1.html';
+  } else if (button.classList.contains("backButton")) {
+    window.location.href='back-day-page-1.html';
+  } else if (button.classList.contains("chestButton")) {
+    window.location.href='chest-day-page-1.html';
+  }
+};
 
-document.querySelector('#nextButton').addEventListener('click', () => {
-  let pageName = document.location.pathname;
-  pageName = pageName.slice(0,-12);
-  updateSelectedExercisesArray();
-  window.location.href=`${pageName}-page-2.html`; // This code can be used for any Exercise Day page written as "exercise-day-page-1.html"
-}); 
+// Add click event to Next Button, to run updateSelectedExercisesArray and then take us to page 3:
+
+function nextButtonToPageTwo(){
+  document.querySelector('#nextButton').addEventListener('click', () => {
+    let pageName = document.location.pathname;
+    pageName = pageName.slice(0,-12);
+    updateSelectedExercisesArray();
+    window.location.href=`${pageName}-page-2.html`; // This code can be used for any Exercise Day page written as "exercise-day-page-1.html"
+  }); 
+};
+
+nextButtonToPageTwo();
+
+// Below we save user inputs:
+
+function saveUserInputs() {
+
+  var inputs = [];
+
+  allExerciseNames.forEach(function(exerciseName){ 
+
+    var setOneName = `${exerciseName}_set_1`;
+    var setOneValue = document.forms[0][setOneName].value;
+    inputs.push(setOneValue);
+
+
+    var setTwoName = `${exerciseName}_set_2`;
+    var setTwoValue = document.forms[0][setTwoName].value;
+    inputs.push(setTwoValue);
+
+
+
+  });
+
+  localStorage.setItem("userSetInputs", JSON.stringify(inputs));
+
+  console.log(localStorage.getItem("userSetInputs"));
+
+};
+
+// Below we retrieve userinputs: 
+
+function getUserInputs() {
+
+  var storedInputs = JSON.parse(localStorage.getItem("userSetInputs"));
+    for (var i = 0; i < 6; i++) {
+      var exerciseName = allLegDayExercises[i];
+      var setOneName = `${exerciseName}_set_1`;
+      document.forms[0][setOneName].value = storedInputs[i*2];
+    }
+
+    for (var i = 0; i < 6; i++) {
+      var exerciseName = allLegDayExercises[i];
+      var setOneName = `${exerciseName}_set_2`;
+      document.forms[0][setOneName].value = storedInputs[i*2+1];
+    }
+
+};
+
+
 
 
 /* Future ideas: 
 
-2. Separate script page for each html page.
 
-1. Create functionality for saving/sending user input in the input fields.
-  a. Use local storage to save input fields, so if someone goes back and forward it'll save their inputted data?
+1. Use local storage to save input fields, so if someone goes back and forward it'll save their inputted data?
 
-- Change function from hiding elements that aren't selected -> adding elements that ARE selected.
-  a. Would make more sense for me and others viewing the code.
-
-3. Future: have one script page that exports main functions to other scripts.
-  a. This way if I ever want to change main function I wouldn't have to go through several scripts manually updating them all, just update one. Also would make each script page much smaller.
-
-- Not sure: allow users to select their exercises list on page 2 from a large database (drop-down list)?
-
-- Email API: Create a hidden input field (similar to contact_number) that can create a large, dynamic text body that pulls variables from JS; only pulling input fields selected, and not all fields. Then use email API to import just this {{ text_body }}.
-
-- Change onclick events on index.html to functions in script.
 
 - Save selected cards and user input even on refresh.
 
-*/
-
-// 1. Create functionality for saving/sending user input in the input fields.
 
 function testSaveInput() {
   var inputt = document.getElementById('test-input').value;
   alert(inputt);
 }
 
+*/
